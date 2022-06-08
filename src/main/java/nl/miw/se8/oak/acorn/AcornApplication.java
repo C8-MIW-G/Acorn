7454cd2f38e3efec410478dac96ae37015dbccc1
Thijs van Blanken <thijs-vb@live.nl>
2022-06-08 15:06:25 +0200

    Initialize project

---
+++ src/main/java/nl/miw/se8/oak/acorn/AcornApplication.java
@@ -0,0 +1,13 @@
+package nl.miw.se8.oak.acorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcornApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miw/se8/oak/acorn/AcornApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miw.se8.oak.acorn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcornApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
