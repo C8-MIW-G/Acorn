d7a23dfa7fd3e2e78e62bab0c7d1704da58c9e28
Thijs van Blanken <thijs-vb@live.nl>
2022-06-27 16:11:23 +0200

    Removed unneccesary methods in PantryUserController

--- src/main/java/nl/miw/se8/oak/acorn/controller/PantryUserController.java
+++ src/main/java/nl/miw/se8/oak/acorn/controller/PantryUserController.java
@@ -1,10 +1,6 @@
 package nl.miw.se8.oak.acorn.controller;
 
-import nl.miw.se8.oak.acorn.model.AcornUser;
-import nl.miw.se8.oak.acorn.model.Pantry;
-import nl.miw.se8.oak.acorn.model.PantryUser;
 import nl.miw.se8.oak.acorn.service.PantryUserService;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -18,15 +14,6 @@ public class PantryUserController {
 
     public PantryUserController(PantryUserService pantryUserService) {
         this.pantryUserService = pantryUserService;
-    }
-
-
-    public void userCreatedANewPantry(AcornUser acornUser, Pantry pantry) {
-        System.out.println(acornUser.getId());
-        System.out.println(acornUser.getName());
-
-        PantryUser pantryCreator = new PantryUser(acornUser, pantry, true);
-        pantryUserService.save(pantryCreator);
     }
 
 }
