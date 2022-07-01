67ec772fa72f544ae29c58e89bf4df2d650cfa23
Thijs van Blanken <thijs-vb@live.nl>
2022-07-01 15:38:59 +0200

    Added validation for unique email when changing users by sysadmins

--- src/main/java/nl/miw/se8/oak/acorn/controller/AdminController.java
+++ src/main/java/nl/miw/se8/oak/acorn/controller/AdminController.java
@@ -9,6 +9,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 /**
@@ -82,21 +85,34 @@ public class AdminController {
         return "redirect:/users";
     }
 
+    // TODO - Fields should autofill after validation messages are shown.
     @PostMapping("users/{userId}/update")
-    protected String userUpdate(@Valid @ModelAttribute("user") UserOverviewVM acornUser, BindingResult result) {
+    protected String userUpdate(@Valid @ModelAttribute("user") UserOverviewVM acornUser,
+                                BindingResult result, RedirectAttributes redirectAttributes) {
         if (result.hasErrors()) {
             return "userDetails";
         }
 
-        Optional<AcornUser> dbAcornUser = acornUserService.findById(acornUser.getId());
+        // TODO - Refactor into separate method
+        // Is email already in use?
+        Optional<AcornUser> dbAcornUser = acornUserService.findByEmail(acornUser.getEmail());
         if (dbAcornUser.isPresent()) {
-            dbAcornUser.get().setEmail(acornUser.getEmail());
-            dbAcornUser.get().setName(acornUser.getName());
-            acornUserService.save(dbAcornUser.get());
-            SecurityController.updatePrincipal(dbAcornUser.get());
+            if (!Objects.equals(dbAcornUser.get().getId(), acornUser.getId())) {
+                FieldError error = new FieldError("user", "email", UserController.ERROR_EMAIL_IN_USE);
+                result.addError(error);
+                return "userDetails";
+            }
         }
+
+        // TODO - Refactor into separate method
+        // Update user in database
+        AcornUser acornUser2 = acornUserService.findById(acornUser.getId()).get();
+        acornUser2.setEmail(acornUser.getEmail());
+        acornUser2.setName(acornUser.getName());
+        acornUserService.save(acornUser2);
+        SecurityController.updatePrincipal(acornUser2);
+
         return "redirect:/users";
     }
-
 
 }
