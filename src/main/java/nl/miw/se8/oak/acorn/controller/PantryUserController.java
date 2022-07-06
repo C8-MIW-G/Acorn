286a966fcc96574328e31630c77c53375410aa6f
wicher <wicherjvos@gmail.com>
2022-07-06 16:10:13 +0200

    Quick commit before pulling

--- src/main/java/nl/miw/se8/oak/acorn/controller/PantryUserController.java
+++ src/main/java/nl/miw/se8/oak/acorn/controller/PantryUserController.java
@@ -5,14 +5,19 @@ import nl.miw.se8.oak.acorn.model.Pantry;
 import nl.miw.se8.oak.acorn.model.PantryUser;
 import nl.miw.se8.oak.acorn.model.ProductDefinition;
 import nl.miw.se8.oak.acorn.service.AcornUserService;
+import nl.miw.se8.oak.acorn.service.AuthorizationService;
 import nl.miw.se8.oak.acorn.service.PantryService;
 import nl.miw.se8.oak.acorn.service.PantryUserService;
 import nl.miw.se8.oak.acorn.viewmodel.Mapper;
 import nl.miw.se8.oak.acorn.viewmodel.PantryMemberVM;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,17 +33,25 @@ public class PantryUserController {
     PantryUserService pantryUserService;
 
     AcornUserService acornUserService;
+    AuthorizationService authorizationService;
 
     PantryService pantryService;
 
-    public PantryUserController(PantryUserService pantryUserService, PantryService pantryService) {
+    public PantryUserController(PantryUserService pantryUserService, PantryService pantryService,
+                                AuthorizationService authorizationService) {
         this.pantryUserService = pantryUserService;
         this.pantryService = pantryService;
+        this.authorizationService = authorizationService;
     }
 
     @GetMapping("/pantry/{pantryId}/members")
     protected String pantryMembers(@PathVariable("pantryId") Long pantryId, Model model) {
         Optional<Pantry> pantry = pantryService.findById(pantryId);
+
+        if (!authorizationService.userCanAccessPantry(pantryId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
         if (pantry.isPresent()) {
             List<PantryUser> pantryUsers = pantryUserService.findPantryUserByPantry(pantry.get());
             List<PantryMemberVM> pantryMemberVMS = new ArrayList<>();
@@ -49,6 +62,12 @@ public class PantryUserController {
             model.addAttribute("pantryMembers", pantryMemberVMS);
         }
         return "pantryMembers";
+    }
+
+    @PostMapping("/pantry/{pantryId}/members/add")
+    protected String addPantryMember(@PathVariable("pantryId") Long pantryId, String newMember, BindingResult result) {
+
+        return null;
     }
 
 
