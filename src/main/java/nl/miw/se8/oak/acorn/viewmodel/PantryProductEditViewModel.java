4dfa5292bac8c9d83bc16b451b992388734493f4
Thijs van Blanken <thijs-vb@live.nl>
2022-07-06 14:23:25 +0200

    AC-70 Modal contains a date input where users can select an expiration date

--- src/main/java/nl/miw/se8/oak/acorn/viewmodel/PantryProductEditViewModel.java
+++ src/main/java/nl/miw/se8/oak/acorn/viewmodel/PantryProductEditViewModel.java
@@ -3,6 +3,8 @@ package nl.miw.se8.oak.acorn.viewmodel;
 import lombok.Getter;
 import lombok.Setter;
 import nl.miw.se8.oak.acorn.model.PantryProduct;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
 /**
@@ -16,6 +18,7 @@ public class PantryProductEditViewModel {
     private Long id;
     private Long pantryId;
     private Long productDefinitionId;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDate expirationDate;
 
     public PantryProductEditViewModel() {
