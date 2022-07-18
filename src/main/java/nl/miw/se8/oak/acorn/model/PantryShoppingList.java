6298d1ad69bc5d75cc0bddf3ddfed4016f0b3d69
Thijs van Blanken <thijs-vb@live.nl>
2022-07-18 13:40:47 +0200

    AC-7 Created PantryShoppingList model

---
+++ src/main/java/nl/miw/se8/oak/acorn/model/PantryShoppingList.java
@@ -0,0 +1,21 @@
+package nl.miw.se8.oak.acorn.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+/**
+ * Author: Thijs van Blanken
+ * Created on: 18-7-2022
+ * Describes a shopping list for a single pantry.
+ */
+@Entity
+public class PantryShoppingList {
+
+    @Id @GeneratedValue
+    private Long id;
+    @OneToOne
+    private Pantry pantry;
+
+}
